88b4efeae0e34a74002d8e150ab59bc07992f331
Desk1 <agv.ispc@gmail.com>
2023-06-18 18:53:08 -0300

    BuberBreakfast

---
+++ BuberBreakfast/Program.cs
@@ -0,0 +1,16 @@
+using BuberBreakfast.Services.Breakfasts;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+}
+
+var app = builder.Build();
+{
+    app.UseExceptionHandler("/error");
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
